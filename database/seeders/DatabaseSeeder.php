f6000a0bf091d92668d66afff51577678a51a993
Pushpak Chhajed <pushpak1300@gmail.com>
2025-10-25 08:34:37 +0530

    Initial commit 🚀

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+final class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::query()->firstOrCreate(['email' => 'test@example.com'], [
+            'name' => 'Test User',
+            'password' => 'password',
+            'email_verified_at' => now(),
+        ]);
+    }
+}
